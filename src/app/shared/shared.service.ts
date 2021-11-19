64828ff8ecd23f8dbc64f1e4c791d5b80acbb087
niljames <njames@dovercorp.com>
2021-11-19 14:45:41 +0530

    updated 1.0

--- src/app/shared/shared.service.ts
+++ src/app/shared/shared.service.ts
@@ -9,24 +9,36 @@ import { IndexedDBService } from '../services/indexed-db.service';
 export class SharedService {
   readonly TicketURL = "http://hackathonvm.centralus.cloudapp.azure.com/ticketingapis/Ticket";
 
-
-  constructor(private http: HttpClient,private indexedDBService:IndexedDBService) { }
+  constructor(private http: HttpClient, private indexedDBService: IndexedDBService) { }
 
   getTicketList(): Observable<any[]> {
-    return this.http.get<any>(this.TicketURL);
+    var user = JSON.parse(sessionStorage.getItem("Access token") || "null");
+    var option = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${user.token}`
+    };
+    console.log(option, "Access token");
+    return this.http.get<any>(this.TicketURL + '/mytickets', { headers: option });
+
   }
 
   getTicketDetails(id: any) {
     return this.indexedDBService.retrieveTicketById(id);
-   
+
   }
 
-  sendSyncData(){
-    var url='http://hackathonvm.centralus.cloudapp.azure.com/ticketingapis/Ticket/Sync';
-    
-   
-    this.indexedDBService.retriveSyncUpdate().then(data=>{
-      return this.http.put(url,data);
+  sendSyncData() {
+    var url = 'http://hackathonvm.centralus.cloudapp.azure.com/ticketingapis/Ticket/Sync';
+
+
+    this.indexedDBService.retriveSyncUpdate().then(data => {
+      console.log(data, "retrieving.");
+      this.http.put(url, data).subscribe(res => {
+        console.log(res, "update");
+        this.indexedDBService.storeSyncUpdate(null);
+      }, err => {
+        console.log(err);
+      });
     });
   }
 
@@ -42,9 +54,9 @@ export class SharedService {
     //   "createdDate": "2021-11-18T07:30:07.823",
     //   "updatedDate": "2021-11-18T11:07:57.03"
     // }
-    var url='http://hackathonvm.centralus.cloudapp.azure.com/ticketingapis/Ticket/Update';
-    console.log("editinggg",val);
-    return this.http.put(url,val);
+    var url = 'http://hackathonvm.centralus.cloudapp.azure.com/ticketingapis/Ticket/Update';
+    console.log("editinggg", val);
+    return this.http.put(url, val);
   }
 
   deleteTicket(id: number) {
